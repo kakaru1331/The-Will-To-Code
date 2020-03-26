570c0709c8aaaf1daf4c075df9bf9e57264583f8
kakaru <kakaru1331@gmail.com>
2020-03-26 23:10:09 +0900

    Added Props sample

--- HelloReactNative/App.tsx
+++ HelloReactNative/App.tsx
@@ -1,14 +1,28 @@
-import React, { Component } from 'react';
-import { Image } from 'react-native';
+import React, { Component, ReactNode } from 'react';
+import { Text, View } from 'react-native';
 
-export default class Bananas extends Component {
+type Props = {
+  name: ReactNode
+}
+
+class Greeting extends Component<Props> {
   render() {
-    let pic = {
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    };
-
     return (
-      <Image source={pic} style={{ width: 193, height: 110 }}></Image>
+      <View style={{ alignItems: 'center' }}>
+        <Text>Hello {this.props.name}!</Text>
+      </View>
+    )
+  }
+}
+
+export default class LotsOfGreetings extends Component {
+  render() {
+    return (
+      <View style={{ alignItems: 'center', top: 50}}>
+        <Greeting name='Rexxar' />
+        <Greeting name='Jaina' />
+        <Greeting name='Valeera' />
+      </View>
     )
   }
 }
