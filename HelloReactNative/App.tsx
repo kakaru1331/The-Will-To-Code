9f3a8129670ef17f6c4fd04d3cfacec3148809a4
kakaru <kakaru1331@gmail.com>
2020-03-26 23:02:49 +0900

    Added Image sample

--- HelloReactNative/App.tsx
+++ HelloReactNative/App.tsx
@@ -1,22 +1,14 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Image } from 'react-native';
 
-export default class HelloWorldApp extends Component {
+export default class Bananas extends Component {
   render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Hello, world!</Text>
-      </View>
+      <Image source={pic} style={{ width: 193, height: 110 }}></Image>
     )
   }
 }
